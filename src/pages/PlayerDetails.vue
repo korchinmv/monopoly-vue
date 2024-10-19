7104e58e260f2617d8830fc25217e39b6183d1e4
Korchin Maxim <98269691+korchinmv@users.noreply.github.com>
2024-10-19 18:21:11 +0300

    Delete src directory

--- src/pages/PlayerDetails.vue
+++
@@ -1,5 +0,0 @@
-<script setup></script>
-
-<template>
-  <div>Details</div>
-</template>
--- src/utils/formatterNum.js
+++
@@ -1,9 +0,0 @@
-export const formatterNum = (num) => {
-  if (num > 1000000) {
-    return (num / 1000000).toFixed(0) + " M";
-  } else if (num > 1000) {
-    return (num / 1000).toFixed(0) + " K";
-  }
-
-  return num.toString();
-};
